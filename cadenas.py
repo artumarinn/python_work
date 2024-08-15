4d8b1da82a1d9f68d234f55367fb3536e5bb4799
Arturo <artumarin.09@gmail.com>
2024-08-15 11:05:52 -0300

    tabulacion

---
+++ cadenas.py
@@ -0,0 +1,8 @@
+print("\tPython") # tabulacion
+print("\nPython") # nueva linea 
+
+favorite_lenguaje = 'Python '
+
+favorite_lenguaje.rstrip() # elimina el espacio en blanco de la derecha
+
+print(favorite_lenguaje) 
--- name.py
+++ name.py
@@ -2,3 +2,4 @@ name = "ada lovelace"
 print(name.title()) # formato de titulo, letra inicial en mayuscula
 print(name.upper()) # MAYUSCULAS
 print(name.lower()) # minusculas
+
