60d90c92d4639e93146e22c102f5be386acaf590
Arturo <artumarin.09@gmail.com>
2024-08-19 00:08:56 -0300

    listas

---
+++ bicycles.py
@@ -0,0 +1,8 @@
+# Listas
+bicycles = ['trek', 'cannondale', 'redline', 'specialiced']
+print(bicycles[0].title()) # se imprime el primer elemento de la lista con la primera letra mayuscula
+
+print(bicycles[-1]) # ultimo valor de la lista 
+
+message = f"My first bicycle was a {bicycles[0]}"
+print(message)
