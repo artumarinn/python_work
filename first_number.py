083f342307e80e3e31d2037ce6f7220aec23aa58
Arturo <artumarin.09@gmail.com>
2024-08-30 12:18:04 -0300

    book: first_number

---
+++ first_number.py
@@ -0,0 +1,12 @@
+# listas numericas
+
+for value in range(1, 5):
+    print(value)
+
+# crear lista    
+number = list(range(1, 5))
+print(number)
+
+# tercer paramertro, suma
+even_numbers =list(range(0, 11, 2))
+print(even_numbers)
