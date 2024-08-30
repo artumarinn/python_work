ca8e9782535ac9a4a00733837657155e31719fea
Arturo <artumarin.09@gmail.com>
2024-08-30 08:11:40 -0300

    exercise: list_bucle finish

--- exercises/list_bucle.py
+++ exercises/list_bucle.py
@@ -2,5 +2,11 @@ pizzas = ['muzzarela', 'fugazzeta', 'pepperoni']
 for pizza in pizzas:
     print(f"I like pizza {pizza.title()}")
     
-print("I love pizza!")
+print("I love pizza!\n")
+
+animals = ['tigre', 'leopardo', 'puma']
+for animal in animals:
+    print(f"{animal.title()} es un animal salvaje")
+
+print("Cualquiera de estos animales seria peligroso")
 
