4a877155cce157b8d2d1f206393b514f37cdc251
Arturo <artumarin.09@gmail.com>
2024-09-03 10:36:28 -0300

    book: first_number

--- first_number.py
+++ first_number.py
@@ -10,3 +10,16 @@ print(number)
 # tercer paramertro, suma
 even_numbers =list(range(0, 11, 2))
 print(even_numbers)
+
+
+squares = []
+for value in range(1, 11):
+    squares.append(value ** 2)
+
+print(squares)
+
+# Max y min de una lista
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(max(digits))
+print(min(digits))
