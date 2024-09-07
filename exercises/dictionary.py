dc421312ee336df6002383d2b0d8c5048fac19c7
Arturo <artumarin.09@gmail.com>
2024-09-06 23:23:27 -0300

    exercise: dictionary

---
+++ exercises/dictionary.py
@@ -0,0 +1,9 @@
+person = { 'first_name': 'arturo', 'last_name': 'marin', 'age': 21, 'city': 'Lujan de Cuyo',}
+
+print(person)
+
+glosario = {'cucumber': 'es un lenguaje de Dominio', 'streamlit': 'sirve para crear webs', 'etc': 'mas facil de cambiar'}
+
+print(f"Cucumber: {glosario['cucumber']}")
+print(f"streamlit: {glosario['streamlit']}")
+print(f"etc: {glosario['etc']}")
