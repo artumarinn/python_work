ca8ac1ed24bec4324049118f5ae63fdbf0fca8d6
artumarinn <artumarin.09@gmail.com>
2024-09-19 22:16:24 -0300

    dictionary: bucle

--- exercises/dictionary.py
+++ exercises/dictionary.py
@@ -7,3 +7,23 @@ person = {
 for key, value in person.items():
     print(f"{key}: {value.title()}")
 
+favorite_lenguage = {
+    'sarah': 'python',
+    'jed': 'java',
+    'edward': 'rust',
+    'arturo': 'python'
+}
+
+for name, lenguage in favorite_lenguage.items():
+    print(f"{name.title()}'s favorite lenguage is {lenguage.title()}")
+    
+if 'jed' in favorite_lenguage.keys(): # keys busca en el diccionario la clave
+        print("GREAT!")
+
+for name in sorted(favorite_lenguage.keys()): # orden las claves 
+    print(f"{name}")
+
+for lenguage in set(favorite_lenguage.values()): # set utiliza la "coleccion" de valores unicos, no imprime duplicados
+     print(f"{lenguage}")
+
+
