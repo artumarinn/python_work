a97149b350e1db4229ea7c317675479e1670ad31
artumarinn <artumarin.09@gmail.com>
2024-09-20 17:08:14 -0300

    dictionary: bucle

---
+++ exercises/dictionary-list.py
@@ -0,0 +1,42 @@
+# diccionarios en listas
+
+persons = []
+
+person01 = {
+    'name' : 'arturo', 
+    'age': '21', 
+    'city': 'mendoza'
+    }
+
+person02 = {
+    'name' : 'martina',
+    'age' : '21',
+    'city' : 'mendoza'
+}
+
+person03 = {
+    'name' : 'graciela',
+    'age' : '52',
+    'city' : 'mendoza'
+}
+
+persons = [person01, person02, person03]
+
+for person in persons:
+    print("Datos personales: ")
+
+
+# diccionarios en diccinarios
+
+ciudades = {
+    
+    'tokio' : {
+        'pais': 'japon',
+        'poblacion': '14 millones'
+        'curiosidades': 'rascacielos iluminados con neones'
+    }
+}
+
+
+
+
