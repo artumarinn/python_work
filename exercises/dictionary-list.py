a2bdb45d55896ec17c01800cd59203c91f0b7470
artumarinn <artumarin.09@gmail.com>
2024-09-21 20:03:41 -0300

    exercise: dictionary-list

--- exercises/dictionary-list.py
+++ exercises/dictionary-list.py
@@ -1,3 +1,4 @@
+'''
 # diccionarios en listas
 
 persons = []
@@ -24,7 +25,7 @@ persons = [person01, person02, person03]
 
 for person in persons:
     print("Datos personales: ")
-
+'''
 
 # diccionarios en diccinarios
 
@@ -32,11 +33,30 @@ ciudades = {
     
     'tokio' : {
         'pais': 'japon',
-        'poblacion': '14 millones'
+        'poblacion': '14 millones',
         'curiosidades': 'rascacielos iluminados con neones'
-    }
+    },
+
+    'mendoza' : {
+        'pais' : 'argentina',
+        'poblacion' : '2 millones',
+        'curiosidades' : 'la mejor produccion de vino' 
+    },
+
+    'cordoba' : {
+        'pais' : 'argentina',
+        'poblacion' : '3.8 millones',
+        'curiosidades' : 'cuidad del fernet'
+    },
 }
 
+for ciudad, info in ciudades.items():
+    print(f"Ciudad: {ciudad.title()}")
+    pais = f"{info['pais']}"
+    poblacion = f"{info['poblacion']}"
+    curiosidades = f"{info['curiosidades']}"
 
-
-
+    print("Informacion: ")
+    print(f"Pais: {pais.title()}")
+    print(f"Poblacion: {poblacion}")
+    print(f"Curiosidades: {curiosidades.title()}\n")
