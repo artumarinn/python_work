994d78dede988d09b4ed0c53a93744ae04b480fc
artumarinn <artumarin.09@gmail.com>
2024-10-02 01:07:21 -0300

    exercises: bucleWhile

--- exercises/bucleWhile.py
+++ exercises/bucleWhile.py
@@ -10,3 +10,29 @@ while pizza:
         break
     else:
         print(f"Usted le agrego: {ingredientes}")
+
+edad = "\nBienvenido al cine"
+edad += "\nIngrese la edad:"
+
+active = True
+while active:
+    edad = input(edad)
+    edad = int(edad)
+
+    if edad == 0:
+        active = False
+        if active == False:
+            break
+
+    if edad < 3:
+        print("Gratis")
+    elif edad > 3 and edad < 12:
+        print("8 euros")
+    else:
+        print("12 euros")
+
+
+# bucle infinito
+number = 2
+while number < 5:
+    print(number)
