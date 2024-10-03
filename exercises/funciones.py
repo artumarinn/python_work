a2a6d59d603d102d76107870e53389aef4eb55e4
artumarinn <artumarin.09@gmail.com>
2024-10-03 02:48:31 -0300

    exercises: funciones

---
+++ exercises/funciones.py
@@ -0,0 +1,10 @@
+def mostrar_mensaje():
+    """Todo lo aprendido en este capitulo"""
+    print("En este capitulo aprendi a como definir una funcion y sobre sus argumentos y parametros")
+
+mostrar_mensaje()
+
+def libro_favorito(nombre_libro):
+    print(f"Mi libro favorito es {nombre_libro}")
+
+libro_favorito("Alicia en el pais de las Maravillas")
